d9f813933a91f21685fbd891ae486f380df24717
Jesse Dirisu <dirisujesse@gmail.com>
2023-04-13 12:14:13 +0100

    Initial Commit

---
+++ Dates/DatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatesApp.swift
+//  Dates
+//
+//  Created by Dirisu on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
